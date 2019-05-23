480955f15812478f181b68d4c0d7e10e4b70c080
CuvéeTRTL Node <leos.stehlik@gmail.com>
2019-05-23 21:00:30 +0000

    initial commit web front-end CuvéeTRTL ARM Pool

---
+++ config.js
@@ -0,0 +1,26 @@
+var api = 'https://publicnode.ydns.eu/api/'
+
+var api_blockexplorer = ''
+
+var poolHost = 'publicnode.ydns.eu'
+
+var irc = ''
+
+var email = ''
+
+var cryptonatorWidget = ['{symbol}-BTC', '{symbol}-USD', '{symbol}-EUR']
+
+var easyminerDownload = 'https://github.com/zone117x/cryptonote-easy-miner/releases/'
+
+var blockchainExplorer = 'https:///{symbol}/block/{id}'
+
+var transactionExplorer = 'https:///{symbol}/transaction/{id}'
+
+var themeCss = 'themes/default-theme.css'
+
+var networkStat = {
+  'bcn': [
+    ['bcn.mypool.online', 'http://bcn.mypool.online:8084'],
+    ['democats.org', 'http://pool.democats.org:7603']
+  ]
+}
